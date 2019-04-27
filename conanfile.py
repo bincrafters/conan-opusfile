617b888e9a605b23c03243097e62a9faa602b70b
SSE4 <tomskside@gmail.com>
2019-04-27 20:05:39 +0700

    - initial
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,73 +1,73 @@
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, MSBuild, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
+class OpusFileConan(ConanFile):
+    name = "opusfile"
+    version = "0.10"
+    description = "stand-alone decoder library for .opus streams"
+    topics = ("conan", "opus", "opusfile", "audio", "decoder", "decoding", "multimedia", "sound")
+    url = "https://github.com/bincrafters/conan-opusfile"
     homepage = "https://github.com/original_author/original_lib"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
+    license = "BSD-3-Clause"
+    exports = ["LICENSE.md"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-
-    # Custom attributes for Bincrafters recipe conventions
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
     requires = (
-        "OpenSSL/1.0.2p@conan/stable",
-        "zlib/1.2.11@conan/stable"
+        "opus/1.2.1@bincrafters/stable",
+        "ogg/1.3.3@bincrafters/stable",
+        "OpenSSL/1.0.2r@conan/stable"
     )
+
+    def configure(self):
+        del self.settings.compiler.libcxx
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
-    def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
-        extracted_dir = self.name + "-" + self.version
+    @property
+    def _is_msvc(self):
+        return self.settings.compiler == "Visual Studio"
 
-        # Rename to "source_subfolder" is a convention to simplify later steps
+    def source(self):
+        source_url = "https://github.com/xiph/opusfile"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version),
+                  sha256="bd9c246cf18d27e9a0815e432731d82f0978717fe2dc2b1e1dce09c184132239")
+        extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def build_vs(self):
+        includedir = os.path.abspath(os.path.join(self._source_subfolder, "include"))
+        with tools.chdir(os.path.join(self._source_subfolder, "win32", "VS2015")):
+            msbuild = MSBuild(self)
+            msbuild.build_env.include_paths.append(includedir)
+            msbuild.build(project_file="opusfile.sln", targets=["opusfile"],
+                          platforms={"x86": "Win32"})
+
+    def build_configure(self):
+        raise Exception("TODO")
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        if self._is_msvc:
+            self.build_vs()
+        else:
+            self.build_configure()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
+        if self._is_msvc:
+            include_folder = os.path.join(self._source_subfolder, "include")
+            self.copy(pattern="*", dst="include", src=include_folder)
+            self.copy(pattern="*.dll", dst="bin", keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["opusfile"]
